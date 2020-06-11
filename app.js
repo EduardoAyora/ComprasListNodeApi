731d4493055cbd63367e88306ab7e1dbb5573bb6
EduardoAyora <eduardoaayora64@gmail.com>
2020-06-11 17:44:35 -0500

    autenticacion con sesion

--- app.js
+++ app.js
@@ -17,7 +17,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const productRouter = require('./routes/productRouter');
 
+
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 var app = express();
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,10 +39,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+  console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
+
 app.use('/products', productRouter);
 
 // catch 404 and forward to error handler
