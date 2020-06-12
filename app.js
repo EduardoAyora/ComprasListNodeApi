dc5dedf31648352895216f62a71e1f627703802c
EduardoAyora <eduardoaayora64@gmail.com>
2020-06-11 19:00:10 -0500

    sesiones con passport

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 
@@ -17,19 +21,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const productRouter = require('./routes/productRouter');
 
-
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-
 var app = express();
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -42,26 +34,29 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
-
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  console.log(req.user);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
--- models/user.js
+++ models/user.js
@@ -1,20 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var passportLocalMongoose = require('passport-local-mongoose');
+
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
     admin:   {
         type: Boolean,
         default: false
     }
 });
+
+// user y password son agregados con el plugin
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
