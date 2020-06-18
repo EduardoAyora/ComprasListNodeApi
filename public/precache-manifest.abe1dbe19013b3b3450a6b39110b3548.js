58ca1b20a470db85f22db242a00d69e3fca1d87e
EduardoAyora <eduardoaayora64@gmail.com>
2020-06-18 12:03:46 -0500

    version con react compilado funciona get products y login recargando

---
+++ public/precache-manifest.abe1dbe19013b3b3450a6b39110b3548.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec0fce3e035ac2fc33f9f6eb657c27e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa34fa4709a46912551",
+    "url": "/static/css/2.a17a1ed7.chunk.css"
+  },
+  {
+    "revision": "4af55b4becf5df66e1b9",
+    "url": "/static/css/main.be8f1a5b.chunk.css"
+  },
+  {
+    "revision": "0fa34fa4709a46912551",
+    "url": "/static/js/2.bbf97760.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.bbf97760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af55b4becf5df66e1b9",
+    "url": "/static/js/main.eeca619d.chunk.js"
+  },
+  {
+    "revision": "966de83e2d44ec42c34b",
+    "url": "/static/js/runtime-main.5f64a9b8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
